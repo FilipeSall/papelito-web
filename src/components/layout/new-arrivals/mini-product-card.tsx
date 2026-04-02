e4559adbd593d7f330783e6406f89a3963647f12
Filipe Salles <filipe.salles@seatecnologia.com.br>
2026-04-02 19:19:43 -0300

    fix: style produt

--- src/components/layout/new-arrivals/mini-product-card.tsx
+++ src/components/layout/new-arrivals/mini-product-card.tsx
@@ -16,8 +16,8 @@ export function MiniProductCard({
   image,
 }: MiniProductCardProps) {
   return (
-    <div className="h-65.5 w-44 shrink-0 snap-start select-none overflow-hidden rounded-2xl border border-[#F3F4F6] bg-white shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)] flex flex-col">
-      <div className="relative h-36 bg-bg-light">
+    <div className="h-65.5 w-44 shrink-0 snap-start select-none overflow-hidden rounded-xl border border-[#F3F4F6] bg-white p-px shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)] flex flex-col items-start">
+      <div className="relative h-36 w-full bg-bg-light">
         {image ? (
           <div className="absolute inset-x-0 top-4 h-28">
             <ImageWithSkeleton
@@ -30,18 +30,23 @@ export function MiniProductCard({
             />
           </div>
         ) : (
-          <ProductImageFallback className="absolute inset-0 rounded-t-2xl" />
+          <ProductImageFallback className="absolute inset-0 rounded-t-xl" />
         )}
 
         <DiscountBadge discount={discount} />
       </div>
 
-      <div className="flex-1 p-3 flex flex-col">
-        <span className="font-black text-xs leading-4 text-brand-dark truncate">
-          {name}
-        </span>
-        <ProductPrice original={originalPrice} current={price} />
-        <AddToCartButton label="Adicionar" />
+      <div className="flex-1 w-full p-3 flex flex-col">
+        <div className="flex flex-col gap-1">
+          <span className="font-black text-xs leading-4 text-brand-dark truncate">
+            {name}
+          </span>
+          <ProductPrice original={originalPrice} current={price} />
+        </div>
+
+        <div className="mt-auto pt-3">
+          <AddToCartButton label="Adicionar" />
+        </div>
       </div>
     </div>
   );
