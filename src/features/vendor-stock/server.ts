9eaa965ba60f94078583ce74d6eabd6165cd4eb3
Filipe Salles <filipe.salles@seatecnologia.com.br>
2026-06-22 07:42:26 -0300

    feat(vendor-stock): add taxonomies service + server barrel exports

--- src/features/vendor-stock/server.ts
+++ src/features/vendor-stock/server.ts
@@ -1,6 +1,13 @@
 export { getVendorStock } from "./services/get-vendor-stock";
+export { getVendorStockTaxonomies } from "./services/get-vendor-stock-taxonomies";
 export type {
   VendorStockFilter,
+  VendorStockFilters,
   VendorStockItem,
   VendorStockSnapshot,
+  VendorStockSort,
+  VendorStockTaxonomies,
+  VendorStockTaxonomyTerm,
+  VendorStockTerm,
 } from "./types/vendor-stock";
+export { VENDOR_STOCK_SORTS } from "./types/vendor-stock";
